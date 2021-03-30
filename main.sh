909c7eb794f6f9a6f8fdb33ef00f16855ee7cdd6
geepair <48669492+geepair@users.noreply.github.com>
2021-03-30 12:09:18 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eux
+
+CITY=Nanchang
+LANGUAGE="zh-CN"
+UNIT=m
+UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
+
+curl \
+  -H "Accept-Language: $LANGUAGE" \
+  -H "User-Agent: $UA" \
+  -o main.html \
+  wttr.in/$CITY?format=4\&$UNIT
