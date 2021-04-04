628db1953abb841c8e47441a4e774069ac3abf54
geepair <48669492+geepair@users.noreply.github.com>
2021-04-04 12:56:45 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@ set -eux
 
 CITY=Nanchang
 LANGUAGE="zh-CN"
-UNIT=m
+UNIT=1mMF
 UA="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
 
 curl \
   -H "Accept-Language: $LANGUAGE" \
   -H "User-Agent: $UA" \
   -o main.html \
-  wttr.in/$CITY?format=4\&$UNIT
+  wttr.in/$CITY?$UNIT
